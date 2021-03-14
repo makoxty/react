96906a53849e1b86bf2f02ed96781ac03d336807
makoxty <m.fukujuu@gmail.com>
2021-03-14 23:43:10 +0900

    React-コンテキストの使い方1

---
+++ react_2_context/src/App.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+import logo from './logo.svg';
+import './App.css';
+
+  let data = {title: 'React-Context',
+  message: 'this is sample message.'}
+
+  const SampleContext = React.createContext(data)
+
+  class App extends Component {
+    newdata = {title: '新しいタイトル',
+    message: '新しいメッセージ'}
+    
+    render() {
+      return <div>
+        <h1 className="bg-primary text-white display-4">React</h1>
+        <div className="container">
+          <Title />
+          <Message />
+          <hr />
+          <SampleContext.Provider value={this.newdata}>
+            <Title />
+            <Message />
+          </SampleContext.Provider>
+          <hr />
+          <Title />
+          <Message />
+        </div>
+      </div>
+    }
+  }
+
+  class Title extends Component {
+    static contextType = SampleContext
+
+    render() {
+      return (
+        <div className="card p-2 my-3">
+          <h2>{this.context.title}</h2>
+        </div>
+      )
+    }
+  }
+
+  class Message extends Component {
+    static contextType = SampleContext
+
+    render() {
+      return (
+        <div className="alert alert-primary">
+          <p>{this.context.message}</p>
+        </div>
+      )
+    }
+  }
+
+export default App;
