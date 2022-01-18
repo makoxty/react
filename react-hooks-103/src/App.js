cd80cdb435a4da7e23177bfdd59a1ba4045c63ff
makoxty <m.fukujuu@gmail.com>
2022-01-18 18:17:29 +0900

    リフレッシュ

---
+++ react-hooks-103/src/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+const App = props => {
+
+  const [name, setName] = useState(props.name);
+  const [price, setPrice] = useState(props.price);
+
+  const reset = () => {
+    setPrice(props.price);
+    setName(props.name);
+  }
+
+  return (
+    <>
+    <p>現在の{name}さんの合計は、{price}円です。</p>
+    <button onClick={() => setPrice(price + 1)}>+1</button>
+    <button onClick={() => setPrice(price - 1)}>-1</button>
+    <button onClick={reset}>Reset</button>
+    <input value={name} onChange={e => setName(e.target.value)} />
+    </>
+  );
+}
+
+App.defaultProps = {
+  name: '',
+  price: 1000
+};
+
+export default App;
